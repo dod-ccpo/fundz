cb09e3b3d9c07f927ed9cb1dd8f311daf4096ecb
richard-dds <richard-ctr@friends.dds.mil>
2018-06-12 11:17:36 -0400

    Update config and scripts

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ from fundz.make_app import make_app, make_config
 
 config = make_config()
 app = make_app(config)
-app.run()
+
+if __name__ == "__main__":
+    app.run(port=config['PORT'])
--- fundz/config.py
+++ fundz/config.py
@@ -2,6 +2,7 @@ def map_config(config):
     return {
         'ENV': config['default']['ENVIRONMENT'],
         'DEBUG': config['default']['DEBUG'],
+        'PORT': int(config['default']['PORT']),
         'SQLALCHEMY_DATABASE_URI': config['default']['DATABASE_URI'],
         'SQLALCHEMY_TRACK_MODIFICATIONS': False
     }
